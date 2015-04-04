6f71e1ada95db0f365fa8a5c67d789595d92e4a5
ellapolo <ellapolo@wharton.upenn.edu>
2015-04-04 15:04:40 -0400

    basic db query

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@
 var moment = require('moment');
 var sentiment = require('sentiment');
 var ejs = require('ejs');
+var oracledb = require('oracledb');
+var dbconfig = require('../dbconfig.js')
+var oracleConnectInfo = {user: dbconfig.user, password: dbconfig.password, connectString: dbconfig.connectString};
   // , fs = require('fs')
   // , wallTemplate = fs.readFileSync(__dirname + '/../views/wall.ejs', 'utf8')
   // , onlineTemplate = fs.readFileSync(__dirname + '/../views/online.ejs', 'utf8');
@@ -21,7 +24,27 @@ exports.example = function(req, res) {
 }
 
 exports.index = function(req, res) {
-	res.render('index');
+	oracledb.getConnection(oracleConnectInfo, function(err, connection) {
+		if (err) {
+			console.log("Error connecting to oracle");
+		} else {
+			connection.execute(
+				"SELECT name "
+				+ "FROM businesses "
+				+ "WHERE ROWNUM < 5",
+				function(err, result) {
+					if (err) {
+						console.log(err);
+					} else {
+						console.log(result);
+						console.log(result.rows);
+						console.log(result.metaData);
+						res.render('index');
+					}
+				});
+		}
+	})
+//	res.render('index');
 }
 
 exports.sentiment_analysis = function(req, res) {
