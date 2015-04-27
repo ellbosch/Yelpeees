ae7d79de81cff85d8ebc70a2b0c3b86aa3f3c43c
Shayan Patel <shayanpatel@levyvlan502.0204.wlan.wireless-resnet.upenn.edu>
2015-04-26 22:53:36 -0400

    added googlemaps node module

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
-
 var moment = require('moment');
 var sentiment = require('sentiment');
 var ejs = require('ejs');
 var oracledb = require('oracledb');
 var dbconfig = require('../dbconfig.js')
 var oracleConnectInfo = {user: dbconfig.user, password: dbconfig.password, connectString: dbconfig.connectString};
-  // , fs = require('fs')
-  // , wallTemplate = fs.readFileSync(__dirname + '/../views/wall.ejs', 'utf8')
-  // , onlineTemplate = fs.readFileSync(__dirname + '/../views/online.ejs', 'utf8');
+var gm = require('googlemaps');
 
 // // We export the init() function to initialize
 // // our KVS values
@@ -24,32 +21,6 @@ exports.example = function(req, res) {
 }
 
 exports.index = function(req, res) {
-	// oracledb.getConnection(oracleConnectInfo, function(err, connection) {
-	// 	if (err) {
-	// 		console.log("Error connecting to oracle");
-	// 	} else {
-	// 		connection.execute(
-	// 			"SELECT name "
-	// 			+ "FROM businesses "
-	// 			+ "WHERE ROWNUM < 5",
-	// 			function(err, result) {
-	// 				if (err) {
-	// 					console.log(err);
-	// 				} else {
-	// 					console.log(result);
-	// 					console.log(result.rows);
-	// 					console.log(result.metaData);
-	// 					res.render('index');
-	// 				}
-	// 			});
-
-	// 		connection.release(function(err) {
-	// 			if (err) {
-	// 				console.log(err);
-	// 			}
-	// 		});
-	// 	}
-	// })
 	res.render('index');
 }
 
@@ -82,7 +53,6 @@ exports.populateSearchResults = function(req, res) {
 				console.log(err);
 			} 
 			else {
-				//console.log("result: " + JSON.stringify(result.rows));
 				var rows = result.rows;
 				for (var i = 0; i < rows.length; i++) {
 					console.log("row " + i);
