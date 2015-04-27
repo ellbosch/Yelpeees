11c961ce86b33c436fd615accfdc181e79cd9100
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2015-04-26 23:29:14 -0400

    changes to index.ejs, merged, some other stuff

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,67 @@
 $(function(){
+	$("#Search").click(function() {
+		$("#error").html("");
+		if ($("input[name='food']").val().trim() == "") {
+			$("#error").html("Empty Food Field");
+		}
+		else {
+			$.ajax({
+				async: true,
+				url: "/search",
+				type: "POST",
+				data: 
+				{
+					"food": $("input[name='food']").val(), 
+					"restaurant":  $("input[name='restaurant']").val() 
+							// "location": $("input[name='location']".val())
+						}// ,
+					// success: function(data) {
+					// 	// console.log("data " + data.success);
+					// 	// data = JSON.parse(data);
+					// 	// if (data.success) {
+					// 	// 	console.log("got data from backend");
+					// 	// 	console.log(data.data);
+					// 	// }
+
+					// },
+					// error: function (xhr, ajaxOptions, thrownError) {
+					// 	console.log("error");
+					// $("#error").html(xhr.responseText);
+					// }
+				});
+		}
+	});
+	
+
+	// get current location
+	var visible = true;
+	var geocoder;
+
+	navigator.geolocation.getCurrentPosition(function (pos) {
+	    var geocoder = new google.maps.Geocoder();
+	    var lat = pos.coords.latitude;
+	    var lng = pos.coords.longitude;
+	    var latlng = new google.maps.LatLng(lat, lng);
+
+	    //reverse geocode the coordinates, returning location information.
+	    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+	        var result = results[0];
+	        var city = '';
+
+	        for (var i = 0, len = result.address_components.length; i < len; i++) {
+	            var ac = result.address_components[i];
+
+	            if (ac.types.indexOf('administrative_area_level_3') >= 0) {
+	                city = ac.short_name;
+	            }
+	        }
+
+	        $('location').val(city);
+
+	    });
+	});
+
+
 	// sentiment analysis
 	function run_sentiment_analysis(text) {
 		$.post("/sentiment", {'text': text}, function(data, status) {
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
-
 var moment = require('moment');
 var sentiment = require('sentiment');
 var ejs = require('ejs');
 var oracledb = require('oracledb');
 var dbconfig = require('../dbconfig.js')
 var oracleConnectInfo = {user: dbconfig.user, password: dbconfig.password, connectString: dbconfig.connectString};
-  // , fs = require('fs')
-  // , wallTemplate = fs.readFileSync(__dirname + '/../views/wall.ejs', 'utf8')
-  // , onlineTemplate = fs.readFileSync(__dirname + '/../views/online.ejs', 'utf8');
+var gm = require('googlemaps');
 
 // // We export the init() function to initialize
 // // our KVS values
@@ -24,32 +21,6 @@ exports.example = function(req, res) {
 }
 
 exports.index = function(req, res) {
-	// oracledb.getConnection(oracleConnectInfo, function(err, connection) {
-	// 	if (err) {
-	// 		console.log("Error connecting to oracle");
-	// 	} else {
-	// 		connection.execute(
-	// 			"SELECT name "
-	// 			+ "FROM businesses "
-	// 			+ "WHERE ROWNUM < 5",
-	// 			function(err, result) {
-	// 				if (err) {
-	// 					console.log(err);
-	// 				} else {
-	// 					console.log(result);
-	// 					console.log(result.rows);
-	// 					console.log(result.metaData);
-	// 					res.render('index');
-	// 				}
-	// 			});
-
-	// 		connection.release(function(err) {
-	// 			if (err) {
-	// 				console.log(err);
-	// 			}
-	// 		});
-	// 	}
-	// })
 	res.render('index');
 }
 
@@ -67,7 +38,7 @@ exports.populateSearchResults = function(req, res) {
 		if (err) {
 			console.log(err.stack);
 		}
-		
+		var location = req.body.location;
 		var restaurant = req.body.restaurant;
 		var food = req.body.food;
 		var sqlRestaurant = "'%" + restaurant.toLowerCase() + "%'";
@@ -76,27 +47,40 @@ exports.populateSearchResults = function(req, res) {
 			+ 			   "FROM businesses B, reviews R "
 			+              "WHERE B.business_id = R.business_id AND LOWER(B.name) LIKE " + sqlRestaurant + " AND LOWER(R.text) LIKE " + sqlFood, 
 
-							{}, {outFormat: oracledb.OBJECT}, function(err, result) {
+							{}, {outFormat: oracledb.OBJECT}, function(err, reviewsResult) {
 			if (err) {
 				console.log("error executing business query");
 				console.log(err);
 			} 
 			else {
-				//console.log("result: " + JSON.stringify(result.rows));
-				var rows = result.rows;
-				for (var i = 0; i < rows.length; i++) {
-					console.log("row " + i);
-					if (i < 10) {
-						console.log("businessName: " + rows[i].NAME + ", review text: " + rows[i].TEXT);
+				connection.execute("SELECT B.name, B.address, T.text, T.tdate "
+			+ 			   "FROM businesses B, tips T "
+			+              "WHERE B.business_id = T.business_id AND LOWER(B.name) LIKE " + sqlRestaurant + " AND LOWER(T.text) LIKE " + sqlFood, 
+
+							{}, {outFormat: oracledb.OBJECT}, function(err, tipsResult) {
+
+					//console.log("result: " + JSON.stringify(result.rows));
+					var reviews = reviewsResult.rows;
+					var tips = tipsResult.rows;
+					var numReviews = reviewsResult.rowsAffected;
+					var numTips = tipsResult.rowsAffected;
+					var count = 0;
+					console.log("num reviews affected: " + numReviews);
+					for (var i = 0; i < reviews.length; i++) {
+						count++
+						if (i < 10) {
+							console.log("businessName: " + reviews[i].NAME + ", review text: " + reviews[i].TEXT);
+						}
 					}
-				}
-				connection.release(function(err) {
-					if (err) {
-						console.log(err);
-						res.render('index');
-					} 
-					req.session.search_results = result.rows;
-					res.send(JSON.stringify({success: true, data: JSON.stringify(result.rows)}));
+					console.log("review count: " + count);
+					connection.release(function(err) {
+						if (err) {
+							console.log(err);
+							res.render('index');
+						} 
+						req.session.search_results = reviewsResult.rows;
+						res.send(JSON.stringify({success: true, data: JSON.stringify(reviewsResult.rows)}));
+					});
 				});
 			}
 		});
