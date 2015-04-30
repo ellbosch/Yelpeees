f395f6e2cb995dd28bc6ed97ea7d967425d06860
ellapolo <ellapolo@wharton.upenn.edu>
2015-04-29 21:22:50 -0400

    minor changes and also a reorg of index.js

--- routes/index.js
+++ routes/index.js
@@ -12,69 +12,11 @@ exports.init = function(callback) {
 	callback();
 };
 
-/**
- * Default index page fetches some content and returns it
- */
-exports.example = function(req, res) {
-	res.render('example');
-	return;
-}
-
 exports.index = function(req, res) {
 	res.render('index');
 }
 
-exports.populateSearchResults = function(req, res) {
-	if (! req.session.search_results) {
-		res.render('/');
-	}
-	else {
-		res.render('search', {results: JSON.stringify(req.session.search_results)});
-	}
-}
 
-function getCloseBusinesses(rows, location, callback) {
-	var result = [];
-	var count = 0;
-	var check = rows.length;
-	for (var i = 0; i < rows.length; i++) {
-		(function(row) {
-			within10Miles(rows[row], location, function(err, isClose) {
-				if (err) {
-					console.log(err);
-					callback(err, null);
-				} else {
-					if (isClose) {
-						result.push(rows[row]);
-						if (row == rows.length - 1) {
-							callback(null, result);
-						}
-					} else {
-						if (row == rows.length - 1) {
-						callback(null, result);
-						}
-					}
-				}
-			});
-		})(i);
-	}
-}
-
-function within10Miles(row, loc2, callback) {
-	var loc1 = row[3] + ", " + row[4];
-	gm.distance(loc1, loc2, function(err, distanceData) {
-		if (err) {
-			callback(err, null);
-		} else {
-			if (!distanceData || distanceData.rows[0].elements[0].status != "OK") {
-				callback(null, false);
-			} else {
-				// TEMPORARY 1000 mile radius until db fully loaded
-				callback(null, parseFloat(distanceData.rows[0].elements[0].distance.text.split(" ")[0].replace(/,/g, '')) < 1609.34);
-			}
-		}
-	});
-}
 
 exports.getBusinesses = function (req, res) {
 	console.log("getting businesses");
@@ -110,30 +52,47 @@ exports.getBusinesses = function (req, res) {
 	});
 }
 
-exports.getReverseGeocode = function(req, res) {
-	var coords = req.body.coords;
 
-	gm.reverseGeocode(coords, function(err, data){
+function getBusinessInfo(businessId, callback) {
+	oracledb.getConnection(oracleConnectInfo, function(err, connection) {
 		if (err) {
-			throw err;
+			console.log("error connecting to db");
+			callback(err, null);
+		} else {
+			connection.execute("SELECT B.name, B.address FROM businesses B WHERE business_id = " + "'" + businessId + "'", 
+				function(err, result) {
+					var info = result.rows;
+					callback(null, info[0]);
+			});
 		}
-	  	var ad = data.results[0].formatted_address;
-	  	var zip = get_zipcode(ad);
-	  	res.send(JSON.stringify({zipcode: zip, address: ad}));
 	});
 }
 
-// get zipcode from address
-function get_zipcode(ad) {
-	var zip = "";
-	var ad_split = ad.split(" ");
-  	if (ad_split.length > 2) {
-	  	var zip_untrim = ad_split[ad_split.length - 2];
-	  	if (zip_untrim.length == 6) {
-		  	zip = zip_untrim.substring(0, zip_untrim.length - 1);
-		}
+function getCloseBusinesses(rows, location, callback) {
+	var result = [];
+	var count = 0;
+	var check = rows.length;
+	for (var i = 0; i < rows.length; i++) {
+		(function(row) {
+			within10Miles(rows[row], location, function(err, isClose) {
+				if (err) {
+					console.log(err);
+					callback(err, null);
+				} else {
+					if (isClose) {
+						result.push(rows[row]);
+						if (row == rows.length - 1) {
+							callback(null, result);
+						}
+					} else {
+						if (row == rows.length - 1) {
+						callback(null, result);
+						}
+					}
+				}
+			});
+		})(i);
 	}
-	return zip;
 }
 
 exports.getGeocode = function(req, res) {
@@ -144,10 +103,12 @@ exports.getGeocode = function(req, res) {
 			throw err;
 		}
 		var location = data.results[0].geometry.location;
-		var zip = get_zipcode(data.results[0].formatted_address);
+		var zip = getZipcode(data.results[0].formatted_address);
 		res.send(JSON.stringify({lat: location.lat, lng: location.lng, zipcode: zip}));
 	});
 }
+
+
 
 exports.getReviewsAndRating = function (req, res) {
 	oracledb.getConnection(oracleConnectInfo, function(err, connection) {
@@ -200,7 +161,7 @@ exports.getReviewsAndRating = function (req, res) {
 										if (i == reviews.length - 1) {
 											if (tips.length == 0) {
 												console.log(reviewTexts);
-												var result = JSON.stringify({reviews:reviewTexts, sentiment:sentiment_analysis(reviewTexts, food), avgStars:sumStars/reviews.length});
+												var result = JSON.stringify({reviews:reviewTexts, sentiment:sentimentAnalysis(reviewTexts, food), avgStars:sumStars/reviews.length});
 												console.log(result);
 												res.send(JSON.stringify({success: true, data: result}));
 											} else {
@@ -214,7 +175,7 @@ exports.getReviewsAndRating = function (req, res) {
 																console.log(err);
 																res.send({success:false, message: "Error querying the database for that food item. Please try again."});
 															} else {
-																var result = JSON.stringify({reviews:reviewTexts, sentiment:sentiment_analysis(reviewTexts, food), avgStars:sumStars/reviews.length});
+																var result = {reviews:reviewTexts, sentiment:sentimentAnalysis(reviewTexts, food), avgStars:sumStars/reviews.length};
 																console.log(result);
 																res.send(JSON.stringify({success: true, data: result}));
 															}
@@ -236,44 +197,43 @@ exports.getReviewsAndRating = function (req, res) {
 }
 
 
-function getBusinessInfo(businessId, callback) {
-	oracledb.getConnection(oracleConnectInfo, function(err, connection) {
+exports.getReverseGeocode = function(req, res) {
+	var coords = req.body.coords;
+
+	gm.reverseGeocode(coords, function(err, data){
 		if (err) {
-			console.log("error connecting to db");
-			callback(err, null);
-		} else {
-			connection.execute("SELECT B.name, B.address FROM businesses B WHERE business_id = " + "'" + businessId + "'", 
-				function(err, result) {
-					var info = result.rows;
-					callback(null, info[0]);
-			});
+			throw err;
 		}
+	  	var ad = data.results[0].formatted_address;
+	  	var zip = getZipcode(ad);
+	  	res.send(JSON.stringify({zipcode: zip, address: ad}));
 	});
 }
 
-function updateUserHistory(userId, businessId, food, callback) {
-	oracledb.getConnection(oracleConnectInfo, function(err, connection) {
-		if (err) {
-			console.log(err.stack);
-			callback(err, null);
-		} else {
-			connection.execute("INSERT INTO history (userid, food_item, business_id) "
-						+ 		"VALUES ('" + userId + "', '" + food + "', '" + businessId + "')" 
-							, function(err, result) {
-				if (err) {
-					callback(err, null);
-				}	else {
-					callback(null, {success:true});
-				}				
-
-			});
+// get zipcode from address
+function getZipcode(ad) {
+	var zip = "";
+	var ad_split = ad.split(" ");
+  	if (ad_split.length > 2) {
+	  	var zip_untrim = ad_split[ad_split.length - 2];
+	  	if (zip_untrim.length == 6) {
+		  	zip = zip_untrim.substring(0, zip_untrim.length - 1);
 		}
-	});
+	}
+	return zip;
 }
 
 
+exports.populateSearchResults = function(req, res) {
+	if (! req.session.search_results) {
+		res.render('/');
+	}
+	else {
+		res.render('search', {results: JSON.stringify(req.session.search_results)});
+	}
+}
 
-function sentiment_analysis(reviews, term) {
+function sentimentAnalysis(reviews, term) {
 
 	var sentiments = [];
 	var minSentiment = Number.MAX_VALUE;
@@ -326,8 +286,40 @@ function analyzeReview(review, searchTerm){
     return avg;
 }
 
-exports.sentiment_analysis = function(req, res) {
-	text = req.body.text;
-	result = sentiment(text);
-	res.send({result: result, success: true});
+
+function updateUserHistory(userId, businessId, food, callback) {
+	oracledb.getConnection(oracleConnectInfo, function(err, connection) {
+		if (err) {
+			console.log(err.stack);
+			callback(err, null);
+		} else {
+			connection.execute("INSERT INTO history (userid, food_item, business_id) "
+						+ 		"VALUES ('" + userId + "', '" + food + "', '" + businessId + "')" 
+							, function(err, result) {
+				if (err) {
+					callback(err, null);
+				}	else {
+					callback(null, {success:true});
+				}				
+
+			});
+		}
+	});
+}
+
+
+function within10Miles(row, loc2, callback) {
+	var loc1 = row[3] + ", " + row[4];
+	gm.distance(loc1, loc2, function(err, distanceData) {
+		if (err) {
+			callback(err, null);
+		} else {
+			if (!distanceData || distanceData.rows[0].elements[0].status != "OK") {
+				callback(null, false);
+			} else {
+				// TEMPORARY 1000 mile radius until db fully loaded
+				callback(null, parseFloat(distanceData.rows[0].elements[0].distance.text.split(" ")[0].replace(/,/g, '')) < 1609.34);
+			}
+		}
+	});
 }
